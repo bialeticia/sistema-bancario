595bc851d1597597dbee12b9933aff13d605532f
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-04-22 20:36:39 -0300

    Melhorias no log de Funcionario

--- src/sistemabancario/Funcionario.java
+++ src/sistemabancario/Funcionario.java
@@ -1,21 +1,24 @@
 package sistemabancario;
+
 import java.util.UUID;
 
 public class Funcionario extends Thread {
-	UUID id = UUID.randomUUID();
-	String nome;
-	Conta contaSalario = new Conta(0, TipoConta.corrente);
-	Conta contaInvestimento = new Conta(0, TipoConta.investimento);
-	Banco banco;
-	boolean recebeuSalario = false;
-	
-	public Funcionario(String nome, Banco banco) {
-		this.nome = nome;
-		this.banco = banco;
-	}
-	
-	public void run() {
-		while (true) {
+    UUID id = UUID.randomUUID();
+    String nome;
+    Banco banco;
+
+    Conta contaSalario = new Conta(0, TipoConta.corrente);
+    Conta contaInvestimento = new Conta(0, TipoConta.investimento);
+    boolean recebeuSalario = false;
+    volatile boolean ativo = true; 
+
+    public Funcionario(String nome, Banco banco) {
+        this.nome = nome;
+        this.banco = banco;
+    }
+
+    public void run() {
+        while (ativo) {
             try {
                 Thread.sleep(1000);
                 double salario = contaSalario.saldo;
@@ -25,11 +28,14 @@ public class Funcionario extends Thread {
                     recebeuSalario = false;
                     SincronizarPrints.log(nome + " transferiu para investimento o valor " + investimento);
                 }
-            } 
-            
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-	}
+        SincronizarPrints.log(nome + " finalizando a thread.");
+    }
+
+    public void desativar() {
+        ativo = false; 
+    }
 }
