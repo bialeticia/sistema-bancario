3dccd037f90357f446c1d098e101e9bdfd4e2053
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-04-22 20:40:24 -0300

    Melhorias nos logs de SincronizarPrints

--- src/sistemabancario/SincronizarPrints.java
+++ src/sistemabancario/SincronizarPrints.java
@@ -1,11 +1,18 @@
 package sistemabancario;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SincronizarPrints {
-	private static final Object lock = new Object();
+    private static final Object lock = new Object();
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     public static void log(String message) {
         synchronized (lock) {
-            System.out.println(message);
+            String timestamp = sdf.format(new Date());
+            String threadName = Thread.currentThread().getName();
+            System.out.println(timestamp + " [" + threadName + "] - " + message);
         }
     }
+
 }
