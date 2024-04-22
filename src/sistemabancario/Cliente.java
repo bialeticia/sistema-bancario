553ab4fa3198c0a41dcc126d906ae83188bb1e5c
Bianca Letícia <biancaleticia.moura@gmail.com>
2024-04-22 20:35:30 -0300

    Melhorias no log de Cliente

--- src/sistemabancario/Cliente.java
+++ src/sistemabancario/Cliente.java
@@ -2,36 +2,43 @@ package sistemabancario;
 import java.util.UUID;
 
 public class Cliente extends Thread {
-	UUID id = UUID.randomUUID();
-	String nome;
-	Conta conta = new Conta(1000, TipoConta.corrente);
-	Loja lojaUm, lojaDois;
-	
-	public Cliente(String nome, Loja lojaUm, Loja lojaDois) {
-		this.nome = nome;
-		this.lojaUm = lojaUm;
+    UUID id = UUID.randomUUID();
+    Conta conta = new Conta(1000, TipoConta.corrente);
+    String nome;
+    Loja lojaUm, lojaDois;
+
+    public Cliente(String nome, Loja lojaUm, Loja lojaDois) {
+        this.nome = nome;
+        this.lojaUm = lojaUm;
         this.lojaDois = lojaDois;
-	}
-	
-	public void run() {
-		double valorCompra;
-		
-		if (Math.random() < 0.5) {
-            valorCompra = 100;
-        } else {
-            valorCompra = 200;
-        }
-		
-		while (conta.saldo > 0) {
+    }
+
+    public void run() {
+        try {
+            double valorCompra;
+
             if (Math.random() < 0.5) {
-                lojaUm.processarCompra(this, valorCompra);
+                valorCompra = 100;
             } else {
-                lojaDois.processarCompra(this, valorCompra);
+                valorCompra = 200;
             }
-            
-            SincronizarPrints.log(nome + " fez uma compra " + valorCompra);
-            SincronizarPrints.log(nome + " novo saldo: " + conta.saldo);
-    		
+
+            while (conta.saldo > 0) {
+                if (Math.random() < 0.5) {
+                    lojaUm.processarCompra(this, valorCompra);
+                    SincronizarPrints.log(nome + ": Fez uma compra na " + lojaUm.nome + " de valor R$ " + valorCompra);
+                } else {
+                    lojaDois.processarCompra(this, valorCompra);
+                    SincronizarPrints.log(nome + ": Fez uma compra na " + lojaDois.nome + " de valor R$ " + valorCompra);
+                }
+
+
+                SincronizarPrints.log(nome + ": Possui um novo saldo de R$: " + conta.saldo);
+
+            }
+        } catch (Exception e) {
+            SincronizarPrints.log("Erro na thread " + Thread.currentThread().getName() + ": " + e.getMessage());
         }
-	}
+
+    }
 }
